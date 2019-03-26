bce9173187476508c07276375e4abd3a39fc3de3
Mendezki <menluca@gmail.com>
2019-03-26 02:15:22 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+    VALUES  ("Ricky Bobby", True),
+            ("Kiwiburger", False),
+            ("Jalapeno Cheeseburger", True),
+            ("Toro Burger", True),
+            ("Veggimate Soy Burger", False),
+            ("Seitanic Burger", True),
+            ("Half a pound Burger", False),
+            ("Southwestern Burger", True).
+            ("Smith Avocado Sandwich", True);
