9afbd66fa69d45aaa490cf0baa16ee5e25c72580
Mendezki <43857047+Mendezki@users.noreply.github.com>
2019-04-10 00:55:13 -0400

    Add files via upload
    bugs fixed

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,8 +1,27 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".changes").on("click", function(event) {
-    var id = $(this).data("id");
+$(function () {
 
+  // Add a burger.
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#new-burger").val().trim(),
+      devoured: 0
+    };
+  
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function () {
+      console.log("Added new burger");
+      location.reload();
+    });
+  });
+
+  $("#changes").on("click", function (event) {
+    event.preventDefault();
+
+    var id = $(this).data("id");
     var devouredState = {
       devoured: 1
     };
@@ -11,47 +30,24 @@ $(function() {
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: devouredState
-    }).then(
-      function() {
-        console.log("changed state to", newDevoured);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-
-    var newBurger = {
-      name: $("#burgern").val().trim(),
-      devoured: 0
-    };
-
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
     }).then(function () {
-      console.log("created new burger");
-      // Reload the page to get the updated burger list.
+      console.log("Burger devoured");
       location.reload();
     });
   });
 
-  $(".delete-burger").on("click", function(event) {
+  $("#delete-burger").on("click", function (event) {
+    event.preventDefault();
+
     var id = $(this).data("id");
 
     // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted burger", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+    $.ajax({
+      type: "DELETE",
+      url: "/api/burgers/" + id
+    }).then(function () {
+      console.log("deleted burger", id);
+      location.reload();
   });
-});
+})
+})
